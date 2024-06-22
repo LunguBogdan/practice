2dbb816889052146166b8b60cda98b31c506becf
bogdan <bogdan@DESKTOP-84H7UIU>
2024-06-22 15:16:04 +0300

    dont net hello world web

---
+++ MyApiApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
