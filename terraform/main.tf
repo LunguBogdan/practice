30f68608b9267169666aa2e7e7de4bd65fe4862f
bogdan <bogdan@DESKTOP-84H7UIU>
2024-06-25 19:45:22 +0300

    Updated terraform file for azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,59 @@
-provider "aws" {
-  region = "us-east-1"
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_instance" "my_api_app" {
-  ami           = "<application_ami_id>"
-  instance_type = "t2.micro"
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
 
-  tags = {
-    Name = "MyApiAppInstance"
+resource "azurerm_virtual_network" "example" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "mySubnet"
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                  = "myVM"
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
+  size                  = "Standard_DS2_v2"
+  admin_username        = "adminuser"
+  admin_password        = "YourAdminPassword"
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = "echo Provisioning completed!"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "myNICConfig"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
